cc7e71f6840f9da6b7a19a176665bac2a1b368b9
Ryan Drachenberg <tssinvestments@gmail.com>
2022-05-17 15:12:39 -0400

    files saving to moralis and ipfs

--- src/components/Mint.js
+++ src/components/Mint.js
@@ -1,5 +1,6 @@
-import React, {useEffect, useRef, useState} from 'react';
+import React, {useEffect, useRef, useState, useCustomHookFetchHook} from 'react';
 import { useMoralis, useMoralisFile} from "react-moralis";
+
 import Moralis from 'moralis';
 
 
@@ -7,47 +8,57 @@ const Mint = () => {
     const { isWeb3Enabled, enableWeb3, isAuthenticated, isWeb3EnableLoading } =
     useMoralis();
 
-    const [file, setFile] = useState('');
+    const [file, setFile] = useState(null);
 
     const {saveFile, moralisFile} = useMoralisFile();
 
     const name = useRef();
     const description = useRef();
-    const imgHash = useRef();
+    const imgHash = useRef(file);
 
     
-   
 
-    
-    // const options = { address: "0x27c5ee7d81a430b12a2aa9c7b19e8cf01944afce", chain: "ropsten"};
-    // let NFTs = await Moralis.Web3API.token.getAllTokenIds(options);
-    // console.log(NFTs)
+    const uploadFile = (file) => {
+        const base64 = file;
+        saveFile(
+            "myfile.txt",
+            {base64},
+            {
+                type: "base64",
+                saveIPFS: true,
+                onSuccess: (result) => console.log(result.ipfs()),
+                onError: (error) => console.log(error)
+            }
+        );
+    };
 
     async function submit(e) {
         e.preventDefault();
         
         console.log('file button clicked')
-
+        console.log(file);
+        // imgHash.current?.click();
+        let imgFile = new Moralis.File(file.name, file);
+        let finalImgFile = await imgFile.saveIPFS();
+        console.log(finalImgFile._url);
+        console.log(finalImgFile.ipfs(), finalImgFile.hash());
+   
+       
         const metaData = {
             name: name.current.value,
             description: description.current.value,
-            image: "/ipfs/" + imgHash.current.value
+            image: 'https://gateway.moralisipfs.com/ipfs/'+ finalImgFile.hash()
         }
 
-        const encodedString = Buffer.from(imgHash.current.value).toString('base64');
-        const encodedStringTwo = Buffer.from(metaData.name).toString('base64');
-        console.log(encodedString)
-        console.log(encodedStringTwo)
         console.log(metaData);
 
-        const imageFile = new Moralis.File(encodedStringTwo, encodedString);
-        await imageFile.saveIPFS().then((res) => {
-            console.log(res);
-        });
-        let imageHash = imageFile.hash();
-
-        saveFile(imageHash);
-        console.log(imageHash);
+        const btoa2 = function(str) {
+            return Buffer.from(str).toString('base64');
+        }
+        const file1 = new Moralis.File(metaData.name+".json", {base64 : btoa2(JSON.stringify(metaData))});
+        
+        let finalFile = await file1.save();
+        console.log(JSON.stringify(finalFile));
     }
     
 
@@ -67,7 +78,9 @@ const Mint = () => {
             
             <div className="form-group">
             <label htmlFor="img-file" id="img-file">Please upload your NFT img file below</label>
-            <input type="file" className="form-control-file" id="exampleFormControlFile1" ref={imgHash}/>
+            <input type="file" className="form-control-file" id="exampleFormControlFile1" onChange={ (e) => {
+                console.log(e.target.files[0])
+                setFile(e.target.files[0])}} ref={imgHash}/>
             </div>
 
             <button className='btn btn-primary' onClick={submit}>Mint</button>
@@ -78,3 +91,13 @@ const Mint = () => {
 }
 
 export default Mint
+
+  // const options = { address: "0x27c5ee7d81a430b12a2aa9c7b19e8cf01944afce", chain: "ropsten"};
+    // let NFTs = await Moralis.Web3API.token.getAllTokenIds(options);
+    // console.log(NFTs)
+
+// const ipfsHash = imgHash.current.value;
+        // await ipfsHash.saveIPFS();
+        // console.log(file)
+        // uploadFile(imgHash.current.files[0]);
+        //const file = new Moralis.File(metaData.name+".json", {base64 : btoa2(JSON.stringify(metaData))});
