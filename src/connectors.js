d8237fcdd5c7f21358acb44d236c8042ec550dab
korrrba <gitcoiner@gmail.com>
2022-06-28 16:50:06 +0200

    connectors: Clean up uns login connector comments.

--- src/connectors.js
+++ src/connectors.js
@@ -6,11 +6,8 @@ export const walletconnect = {provider: 'walletconnect'}
 UAuthMoralisConnector.setUAuthOptions({
   clientID: process.env.REACT_APP_CLIENT_ID,
   redirectUri: process.env.REACT_APP_REDIRECT_URI,
-  fallbackIssuer: undefined,
 
-  // Scope must include openid and wallet
   scope: 'openid wallet',
-  // Injected and walletconnect connectors are required
   connectors: {injected, walletconnect},
 });
 
